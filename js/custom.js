2777340827f36cad5afdb39f96b6ca43c7361a19
molla mehedi <mollameehedi@gmail.com>
2023-12-13 00:27:33 +0600

    home page, reorder, and meterials

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$('.review-slider').slick({
+    centerMode: false,
+    slidesToShow: 2,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
